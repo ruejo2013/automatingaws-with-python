b2d1ba9043629db2536262d487d8e9fb7158f2c5
Patrick R <ruejo2013@gmail.com>
2020-05-26 00:12:36 -0400

    webotoron delete
    Delete the webotron script file from the main file.
    It exist in the 01_webotron file

--- webotron.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-session = boto3.Session(profile_name='pythonAutomation')
-s3 = session.resource('s3')
-
-
-if __name__ == '__main__':
-	for bucket in s3.bucket.all():
-		print(bucket)
